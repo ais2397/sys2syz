3ecc226c44e495ac8aae110f91294fa596a5c1d8
R3x <siddharth.muralee@gmail.com>
2020-07-13 01:09:27 -0700

    C2xml outline

--- core/C2xml.py
+++ core/C2xml.py
@@ -1,2 +1,21 @@
 # Module : C2xml.py 
-# Description : Parses C2xml descriptions
+# Description : Run C2xml and verify the results
+import Utils
+
+import logging
+class C2xml(object):
+    def __init__(self, target):
+        self.target = target
+
+    def run_c2xml(self):
+        # Run C2xml
+        Utils.run_cmd()
+        if (self.verify_xml()):
+            logging.info("[+] C file converted to XML and verified!")
+            return True
+        logging.error("[+] unable to extract the corresponding XML queries")
+        return False
+
+    def verify_xml(self):
+        # Verify whether the output has whatever we expected
+        return True
