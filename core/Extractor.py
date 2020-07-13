0170376f8aebe9d82b32ae606e5fad8257647e4a
R3x <siddharth.muralee@gmail.com>
2020-07-13 01:18:53 -0700

    Extractor outline

---
+++ core/Extractor.py
@@ -0,0 +1,21 @@
+# Module : Extractor.py
+# Description : Extracts the necessary details from the source code
+import Utils
+
+import re
+
+class Extractor(object):
+    def __init__(self, target):
+        self.target = target
+
+    def get_ioclts(self, headers):
+        # Get the headers
+        # use regex to match the IO calls
+        # get the structure names and details
+        return details
+
+    def get_syscalls(self, source):
+        # Get the syscall args
+        # use regex to match the syscall file you used to use
+        # get the details
+        return details
