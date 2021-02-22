d7d480a2df0291c62adf04b2b766b282b1a10c94
Siddharth Muralee <siddharth.muralee@gmail.com>
2021-02-22 22:48:00 +0530

    refactor Extractor

--- core/Extractor.py
+++ core/Extractor.py
@@ -1,108 +1,120 @@
 # Module : Extractor.py
 # Description : Extracts the necessary details from the source code
-from core.Utils import *
+from core.Utils import Utils
+from logger import get_logger
 
-import logging
-from os.path import join
+from os.path import join, basename, isdir, isfile, exists
 import os
 import re
 import collections
 
+
 class Extractor(object):
-    def __init__(self, target):
-        self.target = target
+    io = re.compile(r"#define\s+(.*)\s+_IO\((.*)\).*") # regex for IO_ 
+    iow = re.compile(r"#define\s+(.*)\s+_IOW\((.*),\s+(.*),\s+(.*)\).*") #regex for IOW_
+    ior = re.compile(r"#define\s+(.*)\s+_IOR\((.*),\s+(.*),\s+(.*)\).*") #regex for IOR_
+    iowr = re.compile(r"#define\s+(.*)\s+_IOWR\((.*),\s+(.*),\s+(.*)\).*") #regex for IOWR_
+    macros = re.compile(r"#define\s*\t*([A-Z_0-9]*)\t*\s*.*")
+    more_macros = re.compile(r"#define(\s|\t)+([A-Z_0-9]*)[\t|\s]+(?!_IOWR|_IOR|_IOW|_IO|\()[0-9]*x?[a-z0-9]*")#define(\s|\t)+([A-Z_0-9]*)[\t\s]+([^_IOWR{][0-9]*)")#define(\s|\t)+([^_][A-Z_0-9]*)\t*\s*.*")
+    
+    def __init__(self, sysobj):
+        self.sysobj = sysobj
+        self.target = sysobj.target
         self.files = os.listdir(self.target)
         self.command_macros = []
+        self.logger = get_logger("Extractor", sysobj.log_level)
+
+        self.target_dir = join(os.getcwd(), "out/preprocessed/", basename(self.target))
+        
+        if not exists(self.target_dir):
+            os.mkdir(self.target_dir)
+        self.ioctl_file = ""
+
 
     def get_ioctls(self, header_files):
         """
         Fetch the ioctl commands with their arguments and sort them on the basis of their type
         :return:
         """
+        command_descs = ""
+        ioctl_commands = []
+        command_file = []
         
-        try:
-            command_descs = ""
-            ioctl_commands = []
-            command_file = []
-            for file in header_files:
-                fd = open(self.target + "/" + file, "r")
+        for file in header_files:
+            try:
+                fd = open(join(self.target, file), "r")
                 content = fd.readlines()
                 fd.close()
+            except IOError:
+                self.logger.error("Unable to read the file '%s'", file)
+                self.logger.critical("Skipping this file")
+                continue
 
-                io = re.compile("#define\s+(.*)\s+_IO\((.*)\).*") # regex for IO_ 
-                iow = re.compile("#define\s+(.*)\s+_IOW\((.*),\s+(.*),\s+(.*)\).*") #regex for IOW_
-                ior = re.compile("#define\s+(.*)\s+_IOR\((.*),\s+(.*),\s+(.*)\).*") #regex for IOR_
-                iowr = re.compile("#define\s+(.*)\s+_IOWR\((.*),\s+(.*),\s+(.*)\).*") #regex for IOWR_
-                for line in content:
-                    io_match = io.match(line)
-                    iow_match = iow.match(line)
-                    ior_match = ior.match(line)
-                    iowr_match = iowr.match(line)
+            for line in content:
+                # TODO: realign the matches
+                command_desc = []
+                io_match = self.io.match(line)
+                iow_match = self.iow.match(line)
+                ior_match = self.ior.match(line)
+                iowr_match = self.iowr.match(line)
 
-                    #array to store: PTR direction of ioctl call argument, command macro , command arguments
-                    command_desc = []
-                    if io_match:
-                        command = io_match.groups()[0].strip()
-                        command_desc = ["null", command, "null"] 
-                        command_file.append(file)
-                    elif ior_match:
-                        command = ior_match.groups()[0].strip()
-                        description = ior_match.groups()[-1]
-                        command_desc = ["in", command, description]
-                        command_file.append(file)
-                    elif iow_match:
-                        command = iow_match.groups()[0].strip()
-                        description = iow_match.groups()[-1]
-                        command_desc = ["out", command, description]
-                        command_file.append(file)
-                    elif iowr.match(line):
-                        command = iowr_match.groups()[0].strip()
-                        description = iowr_match.groups()[-1]
-                        command_desc = ["inout", command, description]
-                        command_file.append(file)
-                    if command_desc:
-                        self.command_macros.append(command)
-                        ioctl_commands.append(command_desc)
-                        command_descs += ", ".join(command_desc) + "\n"
-                        command_file.append(file)
-                logging.debug("[*] Analysed " + file)
+                #array to store: PTR direction of ioctl call argument, command macro , command arguments
+                if io_match:
+                    command = io_match.groups()[0].strip()
+                    command_desc = ["null", command, "null"] 
+                    command_file.append(file)
+                elif ior_match:
+                    command = ior_match.groups()[0].strip()
+                    description = ior_match.groups()[-1]
+                    command_desc = ["in", command, description]
+                    command_file.append(file)
+                elif iow_match:
+                    command = iow_match.groups()[0].strip()
+                    description = iow_match.groups()[-1]
+                    command_desc = ["out", command, description]
+                    command_file.append(file)
+                elif iowr_match:
+                    command = iowr_match.groups()[0].strip()
+                    description = iowr_match.groups()[-1]
+                    command_desc = ["inout", command, description]
+                    command_file.append(file)
+                if command_desc:
+                    self.command_macros.append(command)
+                    ioctl_commands.append(command_desc)
+                    command_descs += ", ".join(command_desc) + "\n"
+                    command_file.append(file)
+            self.logger.debug("[*] Analysed " + file)
 
-            if command_descs == "":
-                logging.debug("[*] Doesn't have Ioctl calls")
-                return None, None
-            else:
+        if command_descs == "":
+            self.logger.debug("[*] Doesn't have Ioctl calls")
+            return None
+        else:
+            #store commands in a file named ioctl_commands.txt in the corresponding out/<target> folder 
+            output_file_path = join(os.getcwd(), "out/preprocessed", basename(self.target))
+            if not exists(output_file_path):
+                Utils.create_dir(output_file_path)
 
-                #store commands in a file named ioctl_commands.txt in the corresponding out/<target> folder 
-                output_file_path = os.getcwd() + "/out/preprocessed/" + self.target.split("/")[-1] + "/"
-                if not Utils.dir_exists(output_file_path, False):
-                    os.makedirs(output_file_path)
-                output_file = open(output_file_path + "ioctl_commands.txt", "w")
-                output_file.write(command_descs)
-                logging.debug("[*] Ioctl commands stored at " + output_file_path + "ioctl_commands.txt")
-                output_file.close()
-                return str(output_file_path + "ioctl_commands.txt"), set(command_file)
-        except Exception as e:
-            logging.exception(e)
-            print("Error occurred while Extracting ioctl commands")
-    
+            self.ioctl_file = join(output_file_path, "ioctl_commands.txt") 
+            output_file = open(self.ioctl_file, "w")
+            output_file.write(command_descs)
+            output_file.close()
 
-    def get_header_files(self):
+            self.logger.debug(f"[*] Ioctl commands stored at {output_file_path} ioctl_commands.txt")
+            # TODO: why are we returning
+            return set(command_file)
+
+    @property
+    def header_files(self) -> list:
         """
         Find all the header files in device folder
         :return: list of header files
         """
-
-        try:
-            header_files = []
-            for filename in self.files:
-
-                #store all the filenames ending with ".h" in an array
-                if filename.endswith('.h'):
-                    header_files.append(filename)
-            return header_files
-        except Exception as e:
-            logging.exception(e)
-            print("Error while fetching header files")
+        header_files = []
+        for filename in self.files:
+            #store all the filenames ending with ".h" in an array
+            if filename.endswith('.h'):
+                header_files.append(filename)
+        return header_files
 
 
     def fetch_flags(self, header_files):
@@ -111,29 +123,18 @@ class Extractor(object):
         :return:
         """
 
-        try:
-            #del_buf = open("del_buf", "w")
-            undefined_macros = []
+        undefined_macros = []
+        #read all the files present in target
+        for file in header_files:
+            try:
+                buf = open(join(self.target, file), 'r').read()
+                undefined_macros.extend(self.macros.findall(buf))
+            except IOError:
+                self.logger.error("Unable to open " + join(self.target, file))
 
-            #regex for macros
-            macros = re.compile("#define\s*\t*([A-Z_0-9]*)\t*\s*.*")
-            target = self.target + "/"
-
-            #read all the files present in target
-            for file in header_files:
-                with open(target+file) as fd:
-                    logging.info("target file opened " + target + file)
-                    buf = fd.read()
-                    #del_buf.write(file + "\n--------------\n" + buf)
-
-                    #fetch and store all the macros
-                    undefined_macros.extend(macros.findall(buf))
-
-                #return the macros found, except the IOCTL command macros in header files
-            return list(set(undefined_macros)-set(self.command_macros))
-        except Exception as e:
-            logging.error(e)
-            print("Fails to fetch flags")
+        #TODO: structure this better
+        #return the macros found, except the IOCTL command macros in header files
+        return list(set(undefined_macros)-set(self.command_macros))
 
     def flag_details(self, flags_defined):
         """
@@ -141,54 +142,50 @@ class Extractor(object):
         :return:
         """
 
-        try:
-            all_macros = dict()
-            search_dir = os.getcwd() + "/out/preprocessed/" + self.target.split("/")[-1]
-            macros = re.compile("#define(\s|\t)+([A-Z_0-9]*)[\t|\s]+(?!_IOWR|_IOR|_IOW|_IO|\()[0-9]*x?[a-z0-9]*")#define(\s|\t)+([A-Z_0-9]*)[\t\s]+([^_IOWR{][0-9]*)")#define(\s|\t)+([^_][A-Z_0-9]*)\t*\s*.*")
-            for file in os.listdir(search_dir):
-                if file.endswith(".i"):
-                    with open(join(search_dir, file)) as fd:
-                        logging.debug("target file opened " + join(self.target, file))
-                        # placeholders during iteration
-                        prevline = None
-                        currset = None
-                        currset_start = None
-                        # to hold current file macros
-                        curr_file_macros = []
-                        # Iterate through all the lines in the file
-                        for linenum, line in enumerate(fd.readlines()):
-                            mobj = macros.match(line)
-                            if mobj:
-                                # check if for new set or old set
-                                define_new_set = False
-                                if not prevline:
-                                    prevline = linenum
-                                    define_new_set = True
-                                else:
-                                    if linenum - prevline != 1:
-                                        define_new_set = True
-                                # if we need to define a new set append the older one if exists
-                                # and then create a new one
-                                if define_new_set:
-                                    if currset:
-                                        curr_file_macros.append((currset, currset_start, prevline))
-                                    currset = []
-                                    currset_start = linenum
+        all_macros = dict()
+        for file in filter(lambda x: x if x.endswith(".i") else None, os.listdir(self.target_dir)):
+            try:
+                fd = open(join(self.target_dir, file))
+            except IOError:
+                self.logger.error("Unable to open " + join(self.target_dir, file))
+                continue
+            # placeholders during iteration
+            prevline = None
+            currset = None
+            currset_start = None
+            # to hold current file macros
+            curr_file_macros = []
+            # Iterate through all the lines in the file
+            for linenum, line in enumerate(fd.readlines()):
+                mobj = self.more_macros.match(line)
+                if mobj:
+                    # check if for new set or old set
+                    define_new_set = False
+                    if not prevline:
+                        prevline = linenum
+                        define_new_set = True
+                    else:
+                        if linenum - prevline != 1:
+                            define_new_set = True
+                    # if we need to define a new set append the older one if exists
+                    # and then create a new one
+                    if define_new_set:
+                        if currset:
+                            curr_file_macros.append((currset, currset_start, prevline))
+                        currset = []
+                        currset_start = linenum
 
-                                # Append the set to the old one
-                                macro_name = mobj.group(2)
-                                if macro_name in flags_defined:
-                                    currset.append(mobj.group(2))
-                                    prevline = linenum
-    
-                        all_macros[file] = curr_file_macros
-            return all_macros
-        except Exception as e:
-            logging.error(e)
-            print("Failed to fetch flag details")
+                    # Append the set to the old one
+                    macro_name = mobj.group(2)
+                    if macro_name in flags_defined:
+                        currset.append(mobj.group(2))
+                        prevline = linenum
+
+            all_macros[file] = curr_file_macros
+        return all_macros
 
     def get_syscalls(self, source):
         # Get the syscall args
         # use regex to match the syscall file you used to use
         # get the details
-        return details
+        return self.syscall_details
---
+++ core/logger.py
@@ -0,0 +1,44 @@
+import logging
+from colorlog import ColoredFormatter
+
+def get_logger(name : str, level : int) -> logging.Logger:
+    """returns a Logger instance for a file
+
+    Args:
+        name (str): Name of the logger
+        level (int): Debug level 
+                    (0 -> Error, 1 -> Info, other -> Debug)
+
+    Returns:
+        logging.Logger: Logger Object
+    """
+    l = logging.getLogger(name)
+
+    if level == 0:
+        l.setLevel(logging.ERROR)
+    if level == 1:
+        l.setLevel(logging.INFO)
+    else:
+        l.setLevel(logging.DEBUG)
+
+    stream_h = logging.StreamHandler()
+    file_h = logging.FileHandler('%s.log' % name)
+
+    formatter = ColoredFormatter(
+        "%(asctime)-s %(name)s [%(levelname)s] %(log_color)s%(message)s%(reset)s",
+        datefmt=None, reset=True,
+        log_colors={
+            "DEBUG": "purple",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red",
+        }
+    )
+
+    stream_h.setFormatter(formatter)
+    file_h.setFormatter(formatter)
+    l.addHandler(stream_h)
+    l.addHandler(file_h)
+
+    return l
